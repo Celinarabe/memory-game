11114510d5f523d1e0b62dce81ad2d78362a2d0c
Celina Rabe <celinarabe@yahoo.com>
2021-04-01 22:02:00 -0500

    reduce function

--- src/components/App.js
+++ src/components/App.js
@@ -4,79 +4,74 @@ import Cards from "./Cards";
 import { Container, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-import initializeDeck from '../helpers/initializeDeck';
+import initializeDeck from "../helpers/initializeDeck";
 
 function App() {
-  //returns current state and async function that updates it
-  const [cards, setCards] = useState([{}])
+  //returns current state and async function to update state
+  const [cards, setCards] = useState([{}]);
   const [score, setScore] = useState(0);
   const [highScore, setHighScore] = useState(0);
   const [selected, setSelected] = useState([]);
 
-
   //on component did mount, initialize deck
   useEffect(() => {
-    setCards(initializeDeck())
-    console.log('card deck',cards)
-  }, [])
+    setCards(initializeDeck());
+    console.log("card deck", cards);
+  }, []);
 
   //FOR TESTING TO SEE THE CARDS
   useEffect(() => {
-    console.log('card deck',cards)
-    console.log(cards[0].name)
-  }, [cards])
+    console.log("card deck", cards);
+    console.log(cards[0].name);
+  }, [cards]);
 
-
-  //TO DO: shuffle the card positions on each click
+  //checking if card has already been clicked
   const handleClick = (id) => {
-    if (selected.includes(id)){
-      console.log('already includes')
-      resetScore()
+    if (selected.includes(id)) {
+      console.log("already includes");
+      resetScore();
     } else {
-      addScore()
+      addScore();
     }
-    setSelected(selected => [...selected, id])
-  }
+    setSelected((selected) => [...selected, id]);
+  };
 
-
+  //FOR TESTING TO SEE SELECTED ARRAY
   useEffect(() => {
-    console.log('runs once we add to selected')
-    console.log(selected)
-  }, [selected])
+    console.log(selected);
+  }, [selected]);
 
-  
 
   const addScore = () => {
     setScore(score + 1);
   };
 
-  //TO DO: we need to clear the selected array once the score is reset
+  //TO DO: need to clear the selected array once the score is reset
   const resetScore = () => {
     setScore(0);
-  }
-
+  };
 
   //similar to the componentdidupdate function. the score updater is asynchronous so the state won't be updated until the next render
   //state updates will reflect in the next re-render
   //if you want to perform an action based on state change, you need to use useEffect hook
   useEffect(() => {
-    if (score > highScore){
+    if (score > highScore) {
       setHighScore(score);
     }
-  }, [score])
-
+  }, [score]);
 
   return (
     <div>
       <Container className="text-center">
         <h1>Memory Card Game</h1>
-        <Scoreboard score={score} highScore={highScore} resetScore={resetScore} />
-        {/* <p>{cards[0].id}</p> */}
+        <Scoreboard
+          score={score}
+          highScore={highScore}
+          resetScore={resetScore}
+        />
+        <p>{cards[0].name}</p>
 
-
-        <Cards 
-        addScore={addScore}
-        handleClick={(id) => handleClick(id)} />
+        <Cards addScore={addScore} handleClick={(id) => handleClick(id)} />
       </Container>
     </div>
   );
--- src/helpers/initializeDeck.js
+++ src/helpers/initializeDeck.js
@@ -2,23 +2,18 @@ import cactusArr from "../img/";
 
 //TO DO: Shuffle function
 
-
 //TO DO: array of img names, reduce the array to create list of card objects
 //card object: id and name (full src path)
 
-
 export default function initializeDeck() {
-  console.log('this is being ran')
-  let id=0;
-  let cardDeck = []
+  console.log("this is being ran");
+  let id = 0;
+  let cardDeck = [];
 
-  let test = [{id: 0, name:'celina'}, {id: 1, name: 'yo'}]
-  return test
-  
-  // cactusArr.reduce((acc, val) => {
-  //   let card = {'id': id, 'name': val};
-  //   id++
-  //   cardDeck.push(card)
-  //   return cardDeck
-  // }, cardDeck)
+  return cactusArr.reduce((acc, val) => {
+    let card = { id: id, name: val };
+    id++;
+    cardDeck.push(card);
+    return cardDeck;
+  }, cardDeck);
 }
