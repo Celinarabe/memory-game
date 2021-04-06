d766564c1d7608aaa0b6c51d5d05b8d71484701e
Celina Rabe <celinarabe@yahoo.com>
2021-04-06 17:04:36 -0500

    bootstrap grid

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,24 +1,34 @@
 import React from "react";
 import Image from "react-bootstrap/Image";
+import { Container, Row, Col } from "react-bootstrap";
 import "../css/Cards.css";
 
 function Cards(props) {
-  const generateCards = () => {
-    return props.cardDeck.map((value, index) => {
+  const cardArr = props.cardDeck;
+  const generateCards = (arr) => {
+    return arr.map((value, index) => {
       return (
-        <Image
-          key={value.id}
-          src={value.img}
-          alt={`cactus${value.id}`}
-          onClick={() => props.handleClick(value.id)}
-        />
+        <Col className="px-0 mx-0">
+          <Image
+            key={value.id}
+            src={value.img}
+            alt={`cactus${value.id}`}
+            onClick={() => props.handleClick(value.id)}
+          />
+        </Col>
       );
     });
   };
 
   return (
     <div>
-      <ul>{generateCards()}</ul>
+      <Container fluid>
+        <Row className="justify-content-center">
+          {generateCards(cardArr.slice(0, 3))}
+        </Row>
+        <Row>{generateCards(cardArr.slice(3, 6))}</Row>
+        <Row>{generateCards(cardArr.slice(6, 9))}</Row>
+      </Container>
     </div>
   );
 }
